a35810be7aeec445c7eba641c696b50519bb931d
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-12-10 17:11:44 +0530

    add C++ program to double array values and print the array

--- 02_Array/00_Learning/04_Problems.cpp
+++ 02_Array/00_Learning/04_Problems.cpp
@@ -8,17 +8,27 @@ void takeInput(int arr[], int size){
         cin >> arr[i];
     }
 }
-
 void doubleUpEachValue(int arr[], int size){
+
+    cout << "Doubling the Array Elements...";
     for (int i = 0; i < size; i++){
-        cout << 2 * arr[i] << " ";
+        arr[i] = 2 * arr[i];
+    }
+    cout << endl;
+}
+void printArray(int arr[], int size){
+    cout << "Printing Array..." << endl;
+    for (int i = 0; i < size; i++){
+        cout << arr[i] << " ";
     }
     cout << endl;
 }
 
+
 int main(){
 
     /**
+     * TODO: 
      * 1. Create 10 Size Array 
      * 2. Take input in that array
      * 3. Doubleup each value of Array
@@ -27,7 +37,8 @@ int main(){
     int arr[10];
     takeInput(arr, 10);
     doubleUpEachValue(arr, 10);
+    printArray(arr, 10);
 
-    cout << "Program Completed..." << endl;
+    cout << "Program Completed !!" << endl;
     return 0;
 }
