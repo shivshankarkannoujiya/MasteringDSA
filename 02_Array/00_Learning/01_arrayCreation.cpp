2479158c0cce29c348004d5878f9a6a88beb3a14
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-12-10 16:26:57 +0530

    add C++ program for array creation and address/size demonstration

---
+++ 02_Array/00_Learning/01_arrayCreation.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // TODO: Array creation
+    // int score[10];
+    // char ch[102];
+    // bool flag[101];
+    // long num[111];
+    // short snum[222];
+    // double dNum[300];
+    // float fNum[103];
+
+    // cout << "Array Created Successfully" << endl;
+
+    //TODO: Check Address and Size
+    // int a = 10;
+    // cout << "Address: " << &a << endl;
+    // cout << "Size is: " << sizeof(a) << endl;
+
+    // int arr[10];
+    // cout << "base Address of Arr: " << &arr << endl;
+    // cout << "base Address of Arr: " << arr << endl;
+    // cout << "Size is: " << sizeof(arr) << endl;
+}
