6eda5c02d9fb716ee987ed0caee934f67e9ac8ef
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-12-10 21:44:25 +0530

    add: implement function to find the maximum number in an array

--- 02_Array/01_Easy/01_getMinMax.cpp
+++ 02_Array/01_Easy/01_getMinMax.cpp
@@ -13,9 +13,21 @@ int getMin(int arr[], int size){
     return minNum;
 }
 
+// TODO: find Maximum Number in an Array
+int getMax(int arr[], int size){
+    int maxNumber = INT_MIN;
+    for (int i = 0; i < size; i++){
+        if(arr[i] > maxNumber){
+            maxNumber = arr[i];
+        }
+    }
+    return maxNumber;
+}
 
 int main(){
     int arr[5] = {-1, 1, 3, 4, 5};
     int minNumber = getMin(arr, 5);
+    int maxNumber = getMax(arr, 5);
     cout << "Minimum Number is: " << minNumber << endl;
+    cout << "Maximum Number is: " << maxNumber << endl;
 }
